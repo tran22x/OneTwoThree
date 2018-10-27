a2603e08c2c836c236bcdcf52950b6a1b08e94e5
tran22x <tran22x@mtholyoke.edu>
2018-10-27 19:51:18 -0400

    Added graphics

--- src/Arm.java
+++ src/Arm.java
@@ -1,19 +1,51 @@
+import java.util.LinkedList;
+
 import processing.core.PApplet;
 import processing.core.PVector;
 
 public class Arm {
-
+	private final double MOVEMENT_THRESHOLD = 0.02;
 	public int state = 4;
 	private PApplet app;
-//	private PVector hand;
-//	private PVector shoulder;
-//	private PVector elbow;
+	private PVector hand;
+	private PVector shoulder;
+	private PVector elbow;
+	
+	private PVector preHand;
+	private PVector preShoulder;
+	private PVector preElbow;
+	
+	private LinkedList<PVector> handQueue;
+	private LinkedList<PVector> shoulderQueue;
+	private LinkedList<PVector> elbowQueue;
 	
 	public Arm (PApplet app) {
 		this.app = app;
+		handQueue = new LinkedList<>();
+		shoulderQueue = new LinkedList<>();
+		elbowQueue = new LinkedList<>();
 	}
 	
 	public void draw(PVector hand, PVector elbow, PVector shoulder, PApplet app) {
+		// update position of the arm
+		this.hand = hand;
+		this.elbow = elbow;
+		this.shoulder = shoulder;
+		
+		// push new position into the queue
+		handQueue.add(hand);
+		shoulderQueue.add(shoulder);
+		elbowQueue.add(elbow);
+		
+		if (handQueue.size() > 10) {
+			preHand = handQueue.poll();
+		}
+		if (shoulderQueue.size() > 10) {
+			preShoulder = shoulderQueue.poll();
+		}
+		if (elbowQueue.size() > 10) {
+			preElbow = elbowQueue.poll();
+		}
 		//draw entire arm
 		drawConnection(hand, elbow);
 		drawConnection(elbow, shoulder);
@@ -21,7 +53,11 @@ public class Arm {
 		//draw joint
 		drawJoint (hand);
 		drawJoint (elbow);
-		drawJoint(shoulder);	
+		drawJoint(shoulder);
+		//draw joint
+		drawJoint (preHand);
+		drawJoint (preElbow);
+		drawJoint (preShoulder);
 	}
 	
 	public int getState() {
@@ -49,4 +85,38 @@ public class Arm {
 		}
 	}
 	
+	public boolean isMoving() {
+		boolean isMoving = false;
+		if (preHand!=null && hand!=null) {
+			float difX = Math.abs(preHand.x - hand.x);
+			float difY = Math.abs(preHand.y - hand.y);
+			float difZ = Math.abs(preHand.z - hand.z);
+			isMoving = (difX > MOVEMENT_THRESHOLD || difY > MOVEMENT_THRESHOLD || difZ > MOVEMENT_THRESHOLD);
+			if (isMoving) {
+				System.out.println("movement is detected" + difX);
+				return isMoving;
+			}
+		}
+		if (preShoulder!=null && shoulder!=null) {
+			float difX = Math.abs(preShoulder.x - shoulder.x);
+			float difY = Math.abs(preShoulder.y - shoulder.y);
+			float difZ = Math.abs(preShoulder.z - shoulder.z);
+			isMoving = difX > MOVEMENT_THRESHOLD || difY > MOVEMENT_THRESHOLD || difZ > MOVEMENT_THRESHOLD;
+			if (isMoving) {
+				System.out.println("movement is detected" + difX);
+				return isMoving;
+			}
+		}
+		if (preElbow!=null && elbow!=null) {
+			float difX = Math.abs(preElbow.x - elbow.x);
+			float difY = Math.abs(preElbow.y - elbow.y);
+			float difZ = Math.abs(preElbow.z - elbow.z);
+			isMoving = difX > MOVEMENT_THRESHOLD || difY > MOVEMENT_THRESHOLD || difZ > MOVEMENT_THRESHOLD;
+			if (isMoving) {
+				System.out.println("movement is detected" + difX + " " + difY + " " + difZ);
+				return isMoving;
+			}
+		}
+		return isMoving;
+	}
 }
--- src/WeaponPiece.java
+++ src/WeaponPiece.java
@@ -12,8 +12,13 @@ public class WeaponPiece {
 	private float x;
 	private float y;
 	private PApplet app;
-	private String[] imageArray = {"data/trashempty.png", "data/trashempty.png","data/trashempty.png"};
+	private PImage weapon1;
+	private PImage weapon2;
+	private PImage weapon3;
+	private PImage[] weapons = new PImage[5];
+	private String[] imageArray = {"data/weapon1.png", "data/weapon2.png","data/weapon1","data/weapon2"};
 	private int currImage = 0;
+	private int currWeapon = 0;
 	final double THRESHOLD = .1f;//radius of weapon
 
 	public WeaponPiece(PApplet app) {
@@ -21,14 +26,32 @@ public class WeaponPiece {
 		x = random.nextFloat();
 		y = random.nextFloat();
 		this.app = app;
+		weapon1 = app.loadImage(imageArray[0]);
+		weapon2 = app.loadImage(imageArray[1]);
+		weapon3 = app.loadImage("data/weapon3.png");
+		
+		weapons[0] = weapon1;
+		weapons[1] = weapon2;
+		weapons[2] = weapon3;
+		weapons[3] = weapon2;
+		weapons[4] = weapon1;
+		
 		//drawWeapon();
 		
 	}
 	
 	public void drawWeapon() {
 		//draw the weapon here
-		app.fill(0, 0,255);
-		app.ellipse(x, y, .2f, .2f);
+//		app.fill(0, 0,255);
+//		app.ellipse(x, y, .2f, .2f);
+		if (currWeapon < weapons.length) {
+		//System.out.println("curr weapon is: " + currWeapon);
+		app.image(weapons[currWeapon], x, y, 0.6f, 0.6f);
+		}
+		else {
+			app.fill(0, 0,255);
+			app.ellipse(x, y, .2f, .2f);
+		}
 		//return imageArray[currImage];
 	}
 	
@@ -44,6 +67,7 @@ public class WeaponPiece {
 		//change its location
 		x = random.nextFloat();
 		y = random.nextFloat();
+		currWeapon++;
 		//change its image too
 //		if (imageArray[currImage+1] != null) {
 //			currImage++;
